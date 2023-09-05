d897f36d1f89255afa27843b98490f21712d1153
SuinLee10 <143365454+SuinLee10@users.noreply.github.com>
2023-09-05 23:27:31 +0900

    1step

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -8,8 +8,7 @@ public class WordManager {
     WordCRUD wordCRUD;
 
     public int selectMenu() {
-        System.out.print("*** 영단어 마스터 ***\n"
-                + "********************\n"
+        System.out.print("********************\n"
                 + "1. 모든 단어 보기\n"
                 + "2. 수준별 단어 보기\n"
                 + "3. 단어 검색\n"
@@ -27,6 +26,7 @@ public class WordManager {
         wordCRUD = new WordCRUD(s);
     }
     public void start() {
+        System.out.print("*** 영단어 마스터 ***\n\n");
         while (true) {
             int menu = selectMenu();
             if (menu == 0) break;
@@ -38,5 +38,6 @@ public class WordManager {
                 wordCRUD.addWord();
             }
         }
+        System.out.print("\n프로그램 종료! 다음에 만나~\n");
     }
 }
